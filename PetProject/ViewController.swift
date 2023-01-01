8342d1d23a3dea0b5af548d3d418aec5496099ab
Roman Khilchenko <extrimelife@hotmail.com>
2023-01-01 16:10:40 -0500

    Initial Commit

---
+++ PetProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PetProject
+//
+//  Created by roman Khilchenko on 01.01.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
