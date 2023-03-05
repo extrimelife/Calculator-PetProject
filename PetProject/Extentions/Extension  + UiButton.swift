687588291827d9a515eacf40479e65fe079b11a7
Roman Khilchenko <extrimelife@hotmail.com>
2023-03-05 17:55:31 -0500

    added icon image and chandged animation duration time on the  buttons

--- PetProject/Extentions/Extension  + UiButton.swift
+++ PetProject/Extentions/Extension  + UiButton.swift
@@ -10,8 +10,8 @@ import UIKit
 extension UIButton {
     
     func getAnimation() {
-        UIView.animate(withDuration: 0.2, animations: {
-            self.alpha = 0.5
+        UIView.animate(withDuration: 0.1, animations: {
+            self.alpha = 0.4
         }) { _ in
             UIView.animate(withDuration: 0.2, animations: {
                 self.alpha = 1
